a76706e8331afeedbe109f643a407ed9ce8dccb4
Keith Ort <keith@verticallatency.com>
2018-11-08 15:35:05 -0500

    Added Restaurant Menu to Portfolio

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,10 +2,6 @@ const _ = require('lodash')
 const Promise = require('bluebird')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
-const {
-  createPaginationPages,
-  createLinkedPages,
-} = require('gatsby-pagination')
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
